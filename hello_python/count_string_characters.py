625ded8b3163b4f6aef628047a80d23a5c5bdde8
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-30 23:32:40 +0800

    Removed empty lines

--- hello_python/count_string_characters.py
+++ hello_python/count_string_characters.py
@@ -1,9 +1,11 @@
 user_string_input = input("Enter string: ")
 user_string_input = user_string_input.replace("Enter string: ", '')
 total_byte_in_string_input = len(user_string_input)
+
 byte_dict = {}
+
 for byte in user_string_input:
-	byte_dict[byte] = 0;
+	byte_dict[byte] = 0
 	
 for byte in user_string_input:
 	for key in byte_dict:
@@ -12,6 +14,4 @@ for byte in user_string_input:
 			
 for key in byte_dict:
 	print(f"{key}-{byte_dict[key]}")
-	
-	
 	
