b2ea933a807199968acffba23deb6797286d7eb9
addaguob <141607335+addaguob@users.noreply.github.com>
2023-09-30 23:28:06 +0800

    Adjusted int and string variables

--- hello_c_cpp/area_for_exercises_or_testing.cpp
+++ hello_c_cpp/area_for_exercises_or_testing.cpp
@@ -2,19 +2,19 @@
 
 class A {
   public:
-    int ia;
-    char const * sb = "";
-    int ic;
+    int int_a;
+    char const * string_b = "";
+    int int_c;
   public:
-    void setia(int ia) { }
+    void setia(int int_a) { }
 };
 
 int main() {
   A a;
-  a.ia = 1;
-  a.sb = "Two";
-  a.ic = 3;
-  printf("ia is %d\nsb is %s\nic is %d", a.ia, a.sb, a.ic);
+  a.int_a = 1;
+  a.string_b = "Two";
+  a.int_c = 3;
+  printf("int_a is %d\nsb is %s\nic is %d", a.int_a, a.string_b, a.int_c);
   int x;
   printf("--> %i", x);
   return 0;
