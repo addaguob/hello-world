ebd0732e678f314fbd50a93405809f1a29614e3b
addaguob <141607335+addaguob@users.noreply.github.com>
2023-10-03 20:18:42 +0800

    Modified gui_KTM_notepad geometry

--- hello_python/gui_KTM_notepad.py
+++ hello_python/gui_KTM_notepad.py
@@ -29,6 +29,7 @@ def save_file():
 
 window = tk.Tk()
 window.title("Simple Text Editor")
+window.geometry("700x500")
 
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
@@ -42,6 +43,6 @@ btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
 frm_buttons.grid(row=0, column=0, sticky="ns")
-txt_edit.grid(row=0, column=1, sticky="nsew")
+txt_edit.grid(row=0, column=1, sticky="nsew", ipadx=5, ipady=5)
 
 window.mainloop()
