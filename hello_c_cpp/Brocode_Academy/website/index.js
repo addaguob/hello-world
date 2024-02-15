e55dfe8a2649695f1ea1d62acd22384ed7f5daac
addaguob <141607335+addaguob@users.noreply.github.com>
2024-02-15 23:19:40 +0800

    Arithmetic operators

--- hello_c_cpp/Brocode_Academy/website/index.js
+++ hello_c_cpp/Brocode_Academy/website/index.js
@@ -28,3 +28,16 @@ console.log(isstudent);
 document.getElementById("p1").innerHTML = "Hello " + firstname;
 document.getElementById("p2").innerHTML = "You are " + age + " years old.";
 document.getElementById("p3").innerHTML = "Enrolled: " + isstudent;
+
+// Arithmetic operators
+// + - * / %
+
+let students = 20;
+let extraStudents = students % 2;
+console.log(extraStudents);
+
+extraStudents += 2;
+console.log(extraStudents);
+
+students += 1;
+console.log(students);
