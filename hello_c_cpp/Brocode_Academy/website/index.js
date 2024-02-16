3dcff2bd64c53711c61557f11fbcaa21388719ec
addaguob <141607335+addaguob@users.noreply.github.com>
2024-02-17 00:25:30 +0800

    Explore type-casting

--- hello_c_cpp/Brocode_Academy/website/index.js
+++ hello_c_cpp/Brocode_Academy/website/index.js
@@ -1,5 +1,5 @@
-console.log("I like pizza!");
-console.log("It's really good!");
+// console.log("I like pizza!");
+// console.log("It's really good!");
 
 // window.alert("I really love pizza!");
 
@@ -16,52 +16,74 @@ comment
 // Declaration (var, let, const)
 // Assignmet (= assignment operator)
 
-let firstname = "Alex"; // strings
-let age; // number
-let isstudent = true; // booleans
+// let firstname = "Alex"; // strings
+// let age; // number
+// let isstudent = true; // booleans
 
-age = 32 + 2;
-console.log("Hello", firstname);
-console.log("You are", age, "years old");
-console.log(isstudent);
+// age = 32 + 2;
+// console.log("Hello", firstname);
+// console.log("You are", age, "years old");
+// console.log(isstudent);
 
-document.getElementById("p1").innerHTML = "Hello " + firstname;
-document.getElementById("p2").innerHTML = "You are " + age + " years old.";
-document.getElementById("p3").innerHTML = "Enrolled: " + isstudent;
+// document.getElementById("p1").innerHTML = "Hello " + firstname;
+// document.getElementById("p2").innerHTML = "You are " + age + " years old.";
+// document.getElementById("p3").innerHTML = "Enrolled: " + isstudent;
 
 // Arithmetic operators
 // + - * / %
 // ** is exponent operator
 
-let students = 20;
-let extraStudents = students % 2;
-console.log(extraStudents);
+// let students = 20;
+// let extraStudents = students % 2;
+// console.log(extraStudents);
 
-extraStudents += 2;
-console.log(extraStudents);
+// extraStudents += 2;
+// console.log(extraStudents);
 
-students += 1;
-console.log(students);
+// students += 1;
+// console.log(students);
 
 // Operator precedence
-console.log("Operator precedence=========");
-console.log("1. Parenthesis ()");
-console.log("2. Exponents");
-console.log("3. ultiplication & Dvision");
-console.log("4. Addition & Subtraction");
+// console.log("Operator precedence=========");
+// console.log("1. Parenthesis ()");
+// console.log("2. Exponents");
+// console.log("3. ultiplication & Dvision");
+// console.log("4. Addition & Subtraction");
 
-let result = 1 + 2 * (3 + 4);
-console.log(result);
-result++;
-console.log(result);
+// let result = 1 + 2 * (3 + 4);
+// console.log(result);
+// result++;
+// console.log(result);
 
 // How to accept user input
 // console.log("Easy way user input with a window prompt")
 // let username = window.prompt("What's your name?")
 // console.log(username)
-console.log("Difficult way user input with HTML textbox")
+// console.log("Difficult way user input with HTML textbox")
 let username;
 document.getElementById("mySubmit").onclick = function () {
     username = document.getElementById("myText").value;
     console.log(username)
+    document.getElementById("myH1").textContent = `Hello ${username}`;
 }
+
+// Type conversion
+// let age = window.prompt("How old are you?");
+// // age = Number(age)
+// age += 1;
+// console.log(age, typeof age);
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+document.getElementById("p1").textContent = `x:${x} is ${typeof x}`;
+document.getElementById("p2").textContent = `y:${y} is ${typeof y}`;
+document.getElementById("p3").textContent = `z:${z} is ${typeof z}`;
