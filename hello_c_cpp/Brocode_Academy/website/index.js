9176edcca37640ba5aa29d098943a8f01dfdec0a
addaguob <141607335+addaguob@users.noreply.github.com>
2024-02-22 19:24:37 +0800

    Positional args to funciton

--- hello_c_cpp/Brocode_Academy/website/index.js
+++ hello_c_cpp/Brocode_Academy/website/index.js
@@ -348,39 +348,50 @@ comment
 //     console.log(i);
 // }
 
-console.log("Number guessing game: ")
-const minNum = 1;
-const maxNum = 100;
-const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
-let attempts = 0;
-let guess;
-let running = true;
+// console.log("Number guessing game: ")
+// const minNum = 1;
+// const maxNum = 100;
+// const answer = Math.floor(Math.random() * (maxNum - minNum + 1));
+// let attempts = 0;
+// let guess;
+// let running = true;
 
-console.log(answer);
+// console.log(answer);
 
-while (running) {
-    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
-    guess = Number(guess);
-    console.log(typeof guess, guess);
-    if (isNaN(guess)) {
-        window.alert(`Please enter a valid number`);
-    }
-    else if (guess < minNum || guess > maxNum) {
-        window.alert(`Please enter a number between the specified range`);
+// while (running) {
+//     guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+//     guess = Number(guess);
+//     console.log(typeof guess, guess);
+//     if (isNaN(guess)) {
+//         window.alert(`Please enter a valid number`);
+//     }
+//     else if (guess < minNum || guess > maxNum) {
+//         window.alert(`Please enter a number between the specified range`);
 
-    }
-    else {
-        attempts++;
-        if (guess == answer) {
-            window.alert(`Correct! The answer is ${answer}. Total attempts: ${attempts}`);
-            running = false;
-        }
-        else if (guess > answer) {
-            window.alert(`That's higher than the secret number.`);
-        }
-        else {
-            window.alert(`That's lower than the secret number.`);
-        }
-    }
+//     }
+//     else {
+//         attempts++;
+//         if (guess == answer) {
+//             window.alert(`Correct! The answer is ${answer}. Total attempts: ${attempts}`);
+//             running = false;
+//         }
+//         else if (guess > answer) {
+//             window.alert(`That's higher than the secret number.`);
+//         }
+//         else {
+//             window.alert(`That's lower than the secret number.`);
+//         }
+//     }
+// }
+
+console.log("A function is a section of reusable code");
+function happyBirthday(username, age) {
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log(`Happy birthday dear ${username}!`);
+    console.log("Happy birthday to you!");
+    console.log(`You are ${age} years old!`);
 }
 
+happyBirthday("Alexander", 35);
+happyBirthday("Kryzzabel", 27);
