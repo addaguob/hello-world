01e9fb0d0a10727a1b96fa43bf59f04571963057
addaguob <141607335+addaguob@users.noreply.github.com>
2024-02-24 16:45:24 +0800

    Try generate password

---
+++ hello_web/generate_password/index.js
@@ -0,0 +1,10 @@
+let letters = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "1234567890";
+let symbols = "!@3$%^&*()-_+=/";
+letters += letters.toUpperCase()
+
+console.log(letters);
+
+function generatePassword() {
+
+}
