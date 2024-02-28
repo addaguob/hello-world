036625e928b14c895850b9d00cad3cdc0b6d1c89
addaguob <141607335+addaguob@users.noreply.github.com>
2024-02-28 23:33:50 +0800

    Callback function argument instead of seperate function

--- hello_javascript/_javascript_lectures/index.js
+++ hello_javascript/_javascript_lectures/index.js
@@ -674,11 +674,11 @@ comment
 //     console.log("Hi")
 // }, 3000);
 
-const numbers = [1, 2, 3, 4, 5];
-const squares = numbers.map(square);
+const numbers = [1, 2, 3, 4];
+const squares = numbers.map(function(element) {
+    return Math.pow(element, 2);
+});
 console.log(squares);
 
-function square(element) {
-    return Math.pow(element, 2);
-}
+
 
