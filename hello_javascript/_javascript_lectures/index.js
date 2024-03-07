c5b177c4fc1d311d1c7e89b5cb000b01b18f7495
addaguob <141607335+addaguob@users.noreply.github.com>
2024-03-07 23:38:56 +0800

    Compare two date objects

--- hello_javascript/_javascript_lectures/index.js
+++ hello_javascript/_javascript_lectures/index.js
@@ -1262,3 +1262,12 @@ console.log("Hours:", date.getHours());
 console.log("Minutes:", date.getMinutes());
 console.log("Secs:", date.getSeconds());
 console.log("Day of the week:", date.getDay());
+
+date.setFullYear(2024);
+console.log(date);
+
+const alex_bday = new Date("1988-06-11");
+console.log(alex_bday);
+const kryzza_bday = new Date("1996-08-10"); 
+console.log(kryzza_bday);
+if (alex_bday < kryzza_bday) { console.log("Older") }
