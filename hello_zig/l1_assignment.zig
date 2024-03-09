21ea85687caa2a2e10c45aefa8f9511612e5b71b
addaguob <141607335+addaguob@users.noreply.github.com>
2024-03-09 18:59:38 +0800

    Zig variable placeholders

--- hello_zig/l1_assignment.zig
+++ hello_zig/l1_assignment.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() void {
     const constant: i32 = 5;
     const variable: u32 = 5000;
-    std.debug.print("constant: {i32}!\n", .{constant});
-    std.debug.print("variable: {u32}!\n", .{variable});
+    std.debug.print("constant: {d}!\n", .{constant});
+    std.debug.print("variable: {d}!\n", .{variable});
 }
 // TODO: How to print variables
