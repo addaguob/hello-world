2502402a5f800f786c7794cc5d028bc9713b8763
addaguob <141607335+addaguob@users.noreply.github.com>
2024-03-13 23:17:20 +0800

    Define closure

--- hello_javascript/_javascript_lectures/index.js
+++ hello_javascript/_javascript_lectures/index.js
@@ -1248,31 +1248,33 @@ comment
 // console.log(br, "date object exploration");
 // // Date (year, month, day, hour, minute, second, ms)
 
-// let date = new Date(0); 
+// let date = new Date(0);
 // console.log(date);
 // date = new Date("2024-06-11T12:01:00Z"); // customized
 // console.log(date);
 // date = new Date();
 // console.log(date); // Default is current date
 
-console.log("Year:", date.getFullYear());
-console.log("Month:", date.getMonth());
-console.log("Day:", date.getDate());
-console.log("Hours:", date.getHours());
-console.log("Minutes:", date.getMinutes());
-console.log("Secs:", date.getSeconds());
-console.log("Day of the week:", date.getDay());
+// console.log("Year:", date.getFullYear());
+// console.log("Month:", date.getMonth());
+// console.log("Day:", date.getDate());
+// console.log("Hours:", date.getHours());
+// console.log("Minutes:", date.getMinutes());
+// console.log("Secs:", date.getSeconds());
+// console.log("Day of the week:", date.getDay());
 
-date.setFullYear(2024);
-console.log(date);
+// date.setFullYear(2024);
+// console.log(date);
 
-const alex_bday = new Date("1988-06-11");
-console.log(alex_bday);
-const kryzza_bday = new Date("1996-08-10"); 
-console.log(kryzza_bday);
-if (alex_bday < kryzza_bday) { console.log("Older") }
+// const alex_bday = new Date("1988-06-11");
+// console.log(alex_bday);
+// const kryzza_bday = new Date("1996-08-10");
+// console.log(kryzza_bday);
+// if (alex_bday < kryzza_bday) { console.log("Older") }
 
-let mNum = 3;
-mNum = mNum.toString().padStart(3, 0);
-console.log(mNum);
-console.log(this);
+// let mNum = 3;
+// mNum = mNum.toString().padStart(3, 0);
+// console.log(mNum);
+// console.log(this);
+
+console.log("Closure is a function defined inside another function.")
