28c40d069088aa49f34891cfdd1a017ed1a86751
addaguob <141607335+addaguob@users.noreply.github.com>
2024-03-22 22:54:08 +0800

    Insert coconut before orange

--- hello_javascript/_javascript_lectures/index.js
+++ hello_javascript/_javascript_lectures/index.js
@@ -1576,21 +1576,31 @@ comment
 // Add and change HTML
 
 // Step 1: Create the element
-const newH1 = document.createElement("h1");
+// const newH1 = document.createElement("h1");
+const newListItem = document.createElement("li");
 
 // Step 2: Add attributes and properties
-newH1.textContent = "I like pizza!";
-newH1.id = "myH1";
-newH1.style.color = "tomato";
-newH1.style.textAlign = "center";
+// newH1.textContent = "I like pizza!";
+// newH1.id = "myH1";
+// newH1.style.color = "tomato";
+// newH1.style.textAlign = "center";
+newListItem.textContent = "coconut";
+newListItem.id = "coconut";
+newListItem.style.fontWeight = "bold";
+newListItem.style.backgroundColor = "lightgreen";
 
 // Step 3: Append element to DOM
-// document.body.prepend(newH1);
 // document.body.append(newH1);
-// document.getElementById("box1").prepend(newH1);
+// document.body.prepend(newH1);
+// document.body.prepend(newListItem);
 // document.getElementById("box1").append(newH1);
-const box2 = document.getElementById("box2");
-document.body.insertBefore(newH1, box3);
+// document.getElementById("box1").prepend(newH1);
+document.getElementById("fruits").prepend(newListItem);
+// const box3 = document.getElementById("box3");
+// document.body.insertBefore(newH1, box3);
+// const boxes = document.querySelectorAll(".box");
+// document.body.insertBefore(newH1, boxes[3])
 
 // Remove element
-
+// document.body.removeChild(newH1);
+// document.getElementById("box1").removeChild(newH1);
