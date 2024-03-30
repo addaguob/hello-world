fbda73d56459cdd5f0767f9470215ec747582fa5
addaguob <141607335+addaguob@users.noreply.github.com>
2024-03-30 14:01:06 +0800

    Practice addign semi-colon

--- hello_python/coursera_py4e/2_3_gross_pay.py
+++ hello_python/coursera_py4e/2_3_gross_pay.py
@@ -1,7 +1,5 @@
-hrs = float(input("Enter Hours: "))
-rate = float(input("Enter Rate: "))
-pay = hrs * rate / 1
+hrs = float(input("Enter Hours: "));
+rate = float(input("Enter Rate: "));
+pay = hrs * rate / 1;
 
-print("Pay:", pay)
-
-print ("test")
+print("Pay:", pay);
