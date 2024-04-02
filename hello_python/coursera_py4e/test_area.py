9b1e1b2716112ff344581aa1b758b1ff2fd2ce62
addaguob <141607335+addaguob@users.noreply.github.com>
2024-04-02 18:23:24 +0800

    new file:   hello_python/coursera_py4e/6_5_extract_float.py

--- hello_python/coursera_py4e/test_area.py
+++ hello_python/coursera_py4e/test_area.py
@@ -1,12 +1,14 @@
-big = max("Hello world")
+big = max("Hello world".)
 
 print(big, type(big))
 
 print("Alexander Daguob");
 
 num = 1
-
 if num == '1':
     print("1 and '1' is equal")
 else:
     print("1 and '1' is unequal")
+    
+ustr = u'Alex'
+print(ustr, type(ustr))
