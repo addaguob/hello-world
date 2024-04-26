8197b24a948a9e9b8a1ece59fa4e14eaf2c77884
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2024-04-26 13:47:04 +0800

    Update hello.zig

--- hello_zig/hello.zig
+++ hello_zig/hello.zig
@@ -1,10 +1,10 @@
 // Create a file called main.zig, with the following contents:
 
-// const std = @import("std");
+const std = @import("std");
 
-// pub fn main() void {
-//     std.debug.print("Hello, {s}!\n", .{"World"});
-// }
+pub fn main() void {
+    std.debug.print("Hello, {s}!\n", .{"World"});
+}
 
 // Use zig run main.zig to build and run it. In this example, Hello, World! will be written to stderr, and is assumed to never fail.
 
