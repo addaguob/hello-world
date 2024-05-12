40848d51ea04b53e79c2cd327b5f69d9596cc29c
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-13 00:05:18 +0800

    modified:   hello_python/say.py

--- hello_python/say.py
+++ hello_python/say.py
@@ -7,3 +7,15 @@ this = input("What's this? ")
 cowsay.cow(this)
 engine.say(this)
 engine.runAndWait()
+
+
+num = 1
+
+
+def get_var_name(variable):
+    var_name = [name for name, value in locals().items()
+                if value is variable][0]
+    return var_name
+
+
+print(get_var_name(num))
