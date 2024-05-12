0606436965217457cc55acb7c486d76d32ce07d4
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-13 00:24:37 +0800

    new file:   hello_python/simplegui_practice.py

---
+++ hello_python/simplegui_practice.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+layout = [
+    [sg.Text("Input File:"), sg.Input(key="infile"), sg.FileBrowse()],
+    [sg.Text("Output Folder"), sg.Input(key="outfile"), sg.FolderBrowse()],
+    [sg.Exit(), sg.Button("Convert to CSV")],
+]
+
+window = sg.Window("Excel-CSV Converter", layout)
+
+while True:
+    event, values = window.read()
+    print(event, values)
+    if event in (sg.WINDOW_CLOSED, "Exit"):
+        break
+    if event == "Convert to CSV":
+        sg.popup_error("Oops!", title="Feature not yet implemented")
+window.close()
