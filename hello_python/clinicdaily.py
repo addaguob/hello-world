cb0c76368da7befb926d1abd7935df571b44ae5d
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-19 17:12:49 +0800

    Add more variables

--- hello_python/clinicdaily.py
+++ hello_python/clinicdaily.py
@@ -4,3 +4,10 @@ age = input("Age: ")
 address = input("Contact No: ")
 contact_no = input("Occupation: ")
 date = input("Date: ")
+
+frames = input("Frames: ")
+lens = input("Lens: ")
+total_amount = input("Total Amount: ")
+
+sales_staff = input("Sales Staff: ")
+doctor = input("Doctor: ")
