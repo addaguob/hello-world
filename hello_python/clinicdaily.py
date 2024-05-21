1ba9da9f1030c67f50fad36057772177ae810efa
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-22 04:03:56 +0800

    Add sheet column names as variables for now

--- hello_python/clinicdaily.py
+++ hello_python/clinicdaily.py
@@ -8,6 +8,21 @@ date = input("Date: ")
 frames = input("Frames: ")
 lens = input("Lens: ")
 total_amount = input("Total Amount: ")
+deposit = input("Deposit Amount: ")
+balance = total_amount - deposit
 
 sales_staff = input("Sales Staff: ")
 doctor = input("Doctor: ")
+
+
+# sheet
+
+# table, row per patient (px)
+
+job_order = 0
+px_name = ""
+amount = 0
+deposit = 0
+balance = amount - deposit
+frame = ""
+lens = ""
