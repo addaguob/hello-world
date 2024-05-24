d9109bc72bf23725cb2cd9705aa0f56888e5795e
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2024-05-24 19:08:40 +0800

    Update clinicdaily.py

--- hello_python/clinicdaily.py
+++ hello_python/clinicdaily.py
@@ -3,7 +3,8 @@
 import json
 name = input("Name: ")
 age = input("Age: ")
-address = input("Contact No: ")
+address = input("Address: ")
+contact_no= input("Contact No: ")
 contact_no = input("Occupation: ")
 date = input("Date: ")
 
