e535c7270d3c7886cbfa434b3b857096ee2417c0
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-25 00:10:59 +0800

    modified:   hello_python/clinicdaily.py

--- hello_python/clinicdaily.py
+++ hello_python/clinicdaily.py
@@ -1,46 +1,48 @@
 
-# patient's info
 import json
-name = input("Name: ")
-age = input("Age: ")
-address = input("Contact No: ")
-contact_no = input("Occupation: ")
-date = input("Date: ")
 
-frames = input("Frames: ")
-lens = input("Lens: ")
-total_amount = input("Total Amount: ")
-deposit = input("Deposit Amount: ")
-balance = total_amount - deposit
+# patient's info
 
-sales_staff = input("Sales Staff: ")
-doctor = input("Doctor: ")
+# name = input("Name: ")
+# age = input("Age: ")
+# address = input("Contact No: ")
+# contact_no = input("Occupation: ")
+# date = input("Date: ")
+
+# frames = input("Frames: ")
+# lens = input("Lens: ")
+# total_amount = input("Total Amount: ")
+# deposit = input("Deposit Amount: ")
+# balance = total_amount - deposit
+
+# sales_staff = input("Sales Staff: ")
+# doctor = input("Doctor: ")
 
 
-# sheet/page
+# # sheet/page
 
-# header
-clinic_logo_path = ""
-date = ""
-doctor_in_charge = ""
-sales_personnel = []
+# # header
+# clinic_logo_path = ""
+# date = ""
+# doctor_in_charge = ""
+# sales_personnel = []
 
-# table, row per patient (px)
-job_order = 0
-px_name = ""
-amount = 0
-deposit = 0
-balance = amount - deposit
-frame = ""
-lens = ""
-staff = ""
+# # table, row per patient (px)
+# job_order = 0
+# px_name = ""
+# amount = 0
+# deposit = 0
+# balance = amount - deposit
+# frame = ""
+# lens = ""
+# staff = ""
 
-# footer
-stocks = 0
-sold_items = 0
-added_items = 0
-checked_by = ""
-date_checked = ""
+# # footer
+# stocks = 0
+# sold_items = 0
+# added_items = 0
+# checked_by = ""
+# date_checked = ""
 
 
 px = {
@@ -57,16 +59,26 @@ px = {
 }
 
 print(json.dumps(px))
+print()
+print(px)
+
+try:
+    output_filename = "print_px_not_json.txt"
+    with open(output_filename, "w") as file:
+        file.write(str(px))
+except FileExistsError:
+    with open("02_" + output_filename, "w") as file:
+        file.write(str(px))
 
 lenses = [
-    Ord - Ordinary
-    SV - Single Vision lens
-    MC - Multi-coated
-    BB - Blue Blocker
-    BTS - Blue blocker Transition
-    KK - Kryptok
-    TRG - Transition Gray
-    Prog - Progressive lens
-    Own F - Own Frame
-    F. Only - Frame Only
+    {"Ord" : "Ordinary"},
+    {"SV" : "Single Vision lens"},
+    {"MC" : "Multi-coated"},
+    {"BB" : "Blue Blocker"},
+    {"BTS" : "Blue blocker Transition"},
+    {"KK" : "Kryptok"},
+    {"TRG" : "Transition Gray"},
+    {"Prog" : "Progressive lens"},
+    {"Own F" : "Own Frame"},
+    {"F. Only" : "Frame Only"}
 ]
