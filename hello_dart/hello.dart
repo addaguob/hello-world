e1ed44e5a0ede41554aa15662d342500837fa42b
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-26 00:00:34 +0800

    Variables int and double

--- hello_dart/hello.dart
+++ hello_dart/hello.dart
@@ -1,8 +1,18 @@
 void main() {
+  print("Hello world");
+  print(3);
   // Singe-line comment
+
   /*
   Mutli-line
   comment
   */
-  print('3' * 3 );
+
+  // multiply a string for output
+  print('3' * 3);
+
+  // variables
+  int n1 = 143;
+  double n2 = 41.5;
+  print(n1 + n2);
 }
