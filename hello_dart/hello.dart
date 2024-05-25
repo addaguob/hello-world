c4e01c09fbc862f663f125ce83d8253b6144ff65
addaguob <141607335+addaguob@users.noreply.github.com>
2024-05-26 00:08:45 +0800

    Variables String, bool and dynamin

--- hello_dart/hello.dart
+++ hello_dart/hello.dart
@@ -15,4 +15,16 @@ void main() {
   int n1 = 143;
   double n2 = 41.5;
   print(n1 + n2);
+
+  String fname = "Alex";
+  print(fname);
+
+  bool isMarried = false;
+  isMarried = true;
+  print(isMarried);
+
+  dynamic age = 35;
+  print(age);
+  age = "Thirty-five";
+  print(age);
 }
