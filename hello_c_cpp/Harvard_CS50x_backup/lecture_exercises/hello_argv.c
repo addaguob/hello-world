eb8a727fe189c9f4162bf7e099f95318e0513ac6
addaguob <141607335+addaguob@users.noreply.github.com>
2024-06-06 10:35:40 +0800

    Move files and folders of cs50x

--- hello_c_cpp/Harvard_CS50x_backup/lecture_exercises/hello_argv.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main(int argc, string argv[])
-{
-    printf("Hello, %s!\n", argv[1]);
-    printf("argc is %i!\n", argc);
-    printf("argv[0] is %s and argv[1] is %s!\n", argv[0], argv[1]);
-}
--- hello_c_cpp/Harvard_CS50x_backup/lecture_exercises/operators.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    int x = (4 * 6) / 3;
-    printf("%i\n", x);
-}
