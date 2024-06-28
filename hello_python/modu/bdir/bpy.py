a7239506375f7d9a16867640e22b72f4c9656660
addaguob <141607335+addaguob@users.noreply.github.com>
2024-06-28 14:35:22 +0800

    Case closed on relative import experiments

--- hello_python/modu/bdir/bpy.py
+++ hello_python/modu/bdir/bpy.py
@@ -8,6 +8,9 @@ from adir import apy
 def afunc_via_b():
     print("bpy.afunc_via_b: ", end="")
     apy.afunc()
+    # So far, if this function is imported and called via modu, its parent package,
+    # it works fine
+    # BUT NOT as a script or like below:
     
 if __name__ == "__main__":
     bfunc()
