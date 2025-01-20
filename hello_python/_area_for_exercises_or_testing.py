05525b1d7bc21be92ae3411f39498f250e684be0
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2025-01-20 14:54:07 +0800

    Merge sort playground

--- hello_python/_area_for_exercises_or_testing.py
+++ hello_python/_area_for_exercises_or_testing.py
@@ -1,20 +1,54 @@
 # # These are codes for little experiments
 
-class Head:
-    def __init__(self, eyes):
-        self.eyes = eyes
+def merge_sort(arr):
+    # https://towardsdev.com/algorithms-in-python-2c0c791ef6af
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+data = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(data)
+print("Sorted array:", data)
+
+# class Head:
+#     def __init__(self, eyes):
+#         self.eyes = eyes
     
-    @property
-    def eyes(self):
-        return self._eyes
+#     @property
+#     def eyes(self):
+#         return self._eyes
     
-    @eyes.setter
-    def eyes(self, eyes):
-        self._eyes = eyes
+#     @eyes.setter
+#     def eyes(self, eyes):
+#         self._eyes = eyes
         
     
-mine = Head(2)
-print(mine.eyes)
+# mine = Head(2)
+# print(mine.eyes)
 
 
 # import random
