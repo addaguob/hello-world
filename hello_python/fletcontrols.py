41f7c65aa0117135063cd5f73a18c219080f03ba
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2025-03-05 01:35:33 +0800

    Explore flet tabs

--- hello_python/fletcontrols.py
+++ hello_python/fletcontrols.py
@@ -37,49 +37,49 @@ def main(page: ft.Page):
 
 ft.app(main)
 """
-import flet as ft
+# import flet as ft
 
-def main(page: ft.Page):
-    page.add(
-        ft.DataTable(
-            width=700,
-            bgcolor="yellow",
-            border=ft.border.all(2, "red"),
-            border_radius=10,
-            vertical_lines=ft.BorderSide(3, "blue"),
-            horizontal_lines=ft.BorderSide(1, "green"),
-            sort_column_index=0,
-            sort_ascending=True,
-            heading_row_color=ft.Colors.BLACK12,
-            heading_row_height=100,
-            data_row_color={ft.ControlState.HOVERED: "0x30FF0000"},
-            show_checkbox_column=True,
-            divider_thickness=0,
-            column_spacing=200,
-            columns=[
-                ft.DataColumn(
-                    ft.Text("Column 1"),
-                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
-                ),
-                ft.DataColumn(
-                    ft.Text("Column 2"),
-                    tooltip="This is a second column",
-                    numeric=True,
-                    on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
-                ),
-            ],
-            rows=[
-                ft.DataRow(
-                    [ft.DataCell(ft.Text("A")), ft.DataCell(ft.Text("1"))],
-                    selected=True,
-                    on_select_changed=lambda e: print(f"row select changed: {e.data}"),
-                ),
-                ft.DataRow([ft.DataCell(ft.Text("B")), ft.DataCell(ft.Text("2"))]),
-            ],
-        ),
-    )
+# def main(page: ft.Page):
+#     page.add(
+#         ft.DataTable(
+#             width=700,
+#             bgcolor="yellow",
+#             border=ft.border.all(2, "red"),
+#             border_radius=10,
+#             vertical_lines=ft.BorderSide(3, "blue"),
+#             horizontal_lines=ft.BorderSide(1, "green"),
+#             sort_column_index=0,
+#             sort_ascending=True,
+#             heading_row_color=ft.Colors.BLACK12,
+#             heading_row_height=100,
+#             data_row_color={ft.ControlState.HOVERED: "0x30FF0000"},
+#             show_checkbox_column=True,
+#             divider_thickness=0,
+#             column_spacing=200,
+#             columns=[
+#                 ft.DataColumn(
+#                     ft.Text("Column 1"),
+#                     on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
+#                 ),
+#                 ft.DataColumn(
+#                     ft.Text("Column 2"),
+#                     tooltip="This is a second column",
+#                     numeric=True,
+#                     on_sort=lambda e: print(f"{e.column_index}, {e.ascending}"),
+#                 ),
+#             ],
+#             rows=[
+#                 ft.DataRow(
+#                     [ft.DataCell(ft.Text("A")), ft.DataCell(ft.Text("1"))],
+#                     selected=True,
+#                     on_select_changed=lambda e: print(f"row select changed: {e.data}"),
+#                 ),
+#                 ft.DataRow([ft.DataCell(ft.Text("B")), ft.DataCell(ft.Text("2"))]),
+#             ],
+#         ),
+#     )
 
-ft.app(main)
+# ft.app(main)
 
 
 # import asyncio
@@ -124,3 +124,33 @@ ft.app(main)
 #     page.add(Countdown(120), Countdown(60))
 
 # ft.app(main)
+import flet as ft
+
+def main(page: ft.Page):
+
+    t = ft.Tabs(
+        selected_index=1,
+        animation_duration=300,
+        tabs=[
+            ft.Tab(
+                text="Tab 1",
+                content=ft.Container(
+                    content=ft.Text("This is Tab 1"), alignment=ft.alignment.center
+                ),
+            ),
+            ft.Tab(
+                tab_content=ft.Icon(ft.Icons.SEARCH),
+                content=ft.Text("This is Tab 2"),
+            ),
+            ft.Tab(
+                text="Tab 3",
+                icon=ft.Icons.SETTINGS,
+                content=ft.Text("This is Tab 3"),
+            ),
+        ],
+        expand=1,
+    )
+
+    page.add(t)
+
+ft.app(main)
