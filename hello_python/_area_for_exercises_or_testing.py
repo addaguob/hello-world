26ce0487bac1842bcbc1d88a532e5d8112f4a95e
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2025-04-21 01:50:07 +0800

    Test stdout print buffer with sleep

--- hello_python/_area_for_exercises_or_testing.py
+++ hello_python/_area_for_exercises_or_testing.py
@@ -1,38 +1,46 @@
 # # These are codes for little experiments
+import time
 
-def merge_sort(arr):
-    # https://towardsdev.com/algorithms-in-python-2c0c791ef6af
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left_half = arr[:mid]
-        right_half = arr[mid:]
+print("INFO: logged in", end="")
+time.sleep(1)
 
-        merge_sort(left_half)
-        merge_sort(right_half)
+print("...", end="")
+time.sleep(1)
 
-        i = j = k = 0
-        while i < len(left_half) and j < len(right_half):
-            if left_half[i] < right_half[j]:
-                arr[k] = left_half[i]
-                i += 1
-            else:
-                arr[k] = right_half[j]
-                j += 1
-            k += 1
+print("Done.")
+# def merge_sort(arr):
+#     # https://towardsdev.com/algorithms-in-python-2c0c791ef6af
+#     if len(arr) > 1:
+#         mid = len(arr) // 2
+#         left_half = arr[:mid]
+#         right_half = arr[mid:]
 
-        while i < len(left_half):
-            arr[k] = left_half[i]
-            i += 1
-            k += 1
+#         merge_sort(left_half)
+#         merge_sort(right_half)
 
-        while j < len(right_half):
-            arr[k] = right_half[j]
-            j += 1
-            k += 1
+#         i = j = k = 0
+#         while i < len(left_half) and j < len(right_half):
+#             if left_half[i] < right_half[j]:
+#                 arr[k] = left_half[i]
+#                 i += 1
+#             else:
+#                 arr[k] = right_half[j]
+#                 j += 1
+#             k += 1
 
-data = [38, 27, 43, 3, 9, 82, 10]
-merge_sort(data)
-print("Sorted array:", data)
+#         while i < len(left_half):
+#             arr[k] = left_half[i]
+#             i += 1
+#             k += 1
+
+#         while j < len(right_half):
+#             arr[k] = right_half[j]
+#             j += 1
+#             k += 1
+
+# data = [38, 27, 43, 3, 9, 82, 10]
+# merge_sort(data)
+# print("Sorted array:", data)
 
 # class Head:
 #     def __init__(self, eyes):
