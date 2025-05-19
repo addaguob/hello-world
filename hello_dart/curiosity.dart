76ef614d129de14ec12993fb40bbac19c3fb1d77
Alexander Daguob <141607335+addaguob@users.noreply.github.com>
2025-05-19 20:10:19 +0800

    Failed attempt to explore sizeOf

--- hello_dart/curiosity.dart
+++ hello_dart/curiosity.dart
@@ -1,13 +1,20 @@
 import 'dart:ffi';
+import 'dart:ffi' show calloc;
 
 void main() {
   // Explore python's 28 bytes of int, whereas in C++ it is 4 bytes
   // >>> x = 5
   // >>> import sys
   // >>> print(sys.getsizeof(x), "bytes")
+  // Create a pointer to an int
+  Pointer<Int32> int32 = calloc<Int32>();
+  Pointer<Int64> int64 = calloc<Int64>();
   
-  // Get the size of native types
-  print("Size of native Int8: ${Size.sizeOf<Int8>()} byte(s)");
-  print("Size of native Int32: ${Size.sizeOf<Int32>()} byte(s)");
-  print("Size of native Int64: ${Size.sizeOf<Int64>()} byte(s)");
+  // Get the size of the native types
+  print('Size of Int32: ${sizeOf<Int32>()} bytes');  // Should print 4 bytes
+  print('Size of Int64: ${sizeOf<Int64>()} bytes');  // Should print 8 bytes
+  
+  // Clean up the allocated memory
+  calloc.free(int32);
+  calloc.free(int64);
 }
